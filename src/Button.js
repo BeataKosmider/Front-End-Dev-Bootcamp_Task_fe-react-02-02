2f163625147313b563d5912f3f2dff6469828ea6
Beata Kosmider <kosmibe@gmail.com>
2023-03-17 11:31:46 +0100

    task is done

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-function Button() {
-  return;
+function Button({ onBtnClick, label }) {
+  return <button onClick={onBtnClick}>{label}</button>;
 }
 
 export default Button;
